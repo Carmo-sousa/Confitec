c346c56cb2a6e9f78825b448512fbf2aa66e72d9
Carmo-sousa <carmosousa20@outlook.com>
2022-06-17 00:33:38 -0400

    Delete extencions

--- confitec/blueprints/restapi/resources.py
+++
@@ -1,8 +0,0 @@
-from flask import jsonify
-from flask_restful import Resource
-
-
-class ArtistResource(Resource):
-    def get(self):  # TODO: Implementar esse método!
-        musics = ["Lana", "Matanza"]
-        return jsonify({"musics": musics})
