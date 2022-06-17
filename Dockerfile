3e0d92fd2eee7a4352df1f262d4acbb87c14e51c
Carmo-sousa <carmosousa20@outlook.com>
2022-06-17 15:02:59 -0400

    Remove docker folder to `.gitignore`

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,7 @@ WORKDIR /code
 
 ENV FLASK_RUN_HOST=0.0.0.0
 
-RUN apt update -y && apt upgrade -y && apt install -y gcc musl-dev linux-headers-generic
+RUN apt update -y && apt upgrade -y
 COPY requirements.txt requirements.txt
 
 RUN pip install -r requirements.txt
--- confitec/services/genius.py
+++ confitec/services/genius.py
@@ -5,7 +5,7 @@ import requests
 
 from confitec.ext.database import Artists, resource
 
-cache = redis.Redis(host="localhost", port=6379, decode_responses=True)
+cache = redis.Redis(host="redishost", port=6379, decode_responses=True)
 
 
 class GeniusAPI:
@@ -15,10 +15,17 @@ class GeniusAPI:
 
     def get_songs(self, artist_name, in_cache=True):
         artists = Artists(resource)
-        artist = artists.find_one(artist_name)
 
-        if in_cache and len(artist) > 0:
-            songs = cache.get(artist[0]["id"])
+        if in_cache:
+            songs = cache.get(artist_name.replace(" ", "-"))
+            if not songs:
+                print("Hello", flush=True)
+                response = requests.get(
+                    f"{self.BASE_API}",
+                    params={"q": artist_name},
+                    headers=self.headers,
+                ).json()
+                return response["response"]
             return json.loads(songs)
 
         response = requests.get(
@@ -29,7 +36,7 @@ class GeniusAPI:
 
         if response["meta"]["status"] == 200:
             if not artists.find_one(artist_name):
-                artist_id = artists.save(artist_name)
-                cache.set(artist_id, json.dumps(response["response"]))
+                artists.save(artist_name)
+                cache.set(artist_name.replace(" ", "-"), json.dumps(response["response"]))
 
             return response["response"]
